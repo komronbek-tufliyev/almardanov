6a1609266f276cb5820e3fc8922198d98072b81c
Komronbek Tufliyev <komronbek773@gmail.com>
2023-02-28 21:01:42 +0500

    Look at me

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Navbar extends Component{
+    render(){
+        return(
+            <div>
+                <p>Navbar</p>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
