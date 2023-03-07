8a43140349bd09b8ba1d94fd15ada2490f360ed2
komronbek-tufliyev <komronbek773@gmail.com>
2023-03-07 14:38:25 +0500

    ksla mn

--- almardanov-beta/frontend/src/components/Navbar.jsx
+++ almardanov-beta/frontend/src/components/Navbar.jsx
@@ -5,7 +5,30 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+          <Link to="/" className="navbar-brand">
+            Exercise Tracker
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Exercises
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">
+                  Create Exercise Log
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/user" className="nav-link">
+                  Create User
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- almardanov-beta/frontend/src/index.js
+++ almardanov-beta/frontend/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+import Navbar from './components/Navbar';
 
-
-import Navbar from './components/Navbar2';
-import HomePage from './views/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <HomePage />
   </React.StrictMode>
 );
 
