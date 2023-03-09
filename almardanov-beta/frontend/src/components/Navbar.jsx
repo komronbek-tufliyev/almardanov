e0a9ef3bc6d23c8c20a25ae0f6925ad8de6c06bd
komronbek-tufliyev <komronbek773@gmail.com>
2023-03-09 13:52:13 +0500

    mnk jn sc'

--- almardanov-beta/frontend/src/components/Navbar.jsx
+++ almardanov-beta/frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function NavScrollExample() {
   return (
     <Navbar bg="dark" variant='dark' expand="lg">
@@ -47,42 +47,45 @@ function NavScrollExample() {
       </Container>
     </Navbar>
   );
-=======
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          <Link to="/" className="navbar-brand">
-            Exercise Tracker
-          </Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">
-                  Exercises
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/create" className="nav-link">
-                  Create Exercise Log
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/user" className="nav-link">
-                  Create User
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
->>>>>>> 8a43140349bd09b8ba1d94fd15ada2490f360ed2
 }
+
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+//           <Link to="/" className="navbar-brand">
+//             Exercise Tracker
+//           </Link>
+//           <div className="collapse navbar-collapse">
+//             <ul className="navbar-nav mr-auto">
+//               <li className="navbar-item">
+//                 <Link to="/" className="nav-link">
+//                   Exercises
+//                 </Link>
+//               </li>
+//               <li className="navbar-item">
+//                 <Link to="/create" className="nav-link">
+//                   Create Exercise Log
+//                 </Link>
+//               </li>
+//               <li className="navbar-item">
+//                 <Link to="/user" className="nav-link">
+//                   Create User
+//                 </Link>
+//               </li>
+//             </ul>
+//           </div>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+
+
 
 export default NavScrollExample;
--- almardanov-beta/frontend/src/index.js
+++ almardanov-beta/frontend/src/index.js
@@ -10,18 +10,12 @@ import QuizPage from './components/QuizPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 8a43140349bd09b8ba1d94fd15ada2490f360ed2
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-<<<<<<< HEAD
     <QuizPage />
-=======
->>>>>>> 8a43140349bd09b8ba1d94fd15ada2490f360ed2
   </React.StrictMode>
 );
 
